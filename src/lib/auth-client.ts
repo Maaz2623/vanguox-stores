6f7ec8d3fa752e5c141a1e9eb3b46a744e34c8f2
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-23 15:17:22 +0530

    modified:   src/lib/auth-client.ts

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,12 @@
 import { createAuthClient } from "better-auth/react";
+
+const isProduction = process.env.NODE_ENV === "production";
+
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
+  baseURL: isProduction
+    ? process.env.NEXT_PUBLIC_APP_URL
+    : "http://localhost:3000",
 });
 
-export const { signIn } = authClient;
+export const { signIn, signOut } = authClient;
