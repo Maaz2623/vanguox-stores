d1a1ac536fe4471ab712b737fb1f41d05bad9f71
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-28 09:43:24 +0530

    modified:   src/app/(main)/(stores)/stores/[storeName]/layout.tsx 	modified:   src/app/(main)/(stores)/stores/[storeName]/page.tsx

--- src/app/(main)/(stores)/stores/[storeName]/page.tsx
+++ src/app/(main)/(stores)/stores/[storeName]/page.tsx
@@ -9,5 +9,13 @@ interface PageProps {
 export default async function Page({ params }: PageProps) {
   const { storeName } = await params;
   console.log(storeName);
-  return <SectionCards />;
+  return (
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 md:gap-6">
+          <SectionCards />
+        </div>
+      </div>
+    </div>
+  );
 }
