870aea1641f93953b4c989f71aac259df3bc4c3b
Mohammed Maaz <mohammedmaaz2623@gmail.com>
2025-05-28 21:12:00 +0530

    modified:   src/trpc/routers/_app.ts 	modified:   src/trpc/server.tsx

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,8 @@ import { createTRPCRouter } from "../init";
 import { productImagesRouter } from "../procedures/product-images.procedure";
 import { productsRouter } from "../procedures/products.procedure";
 import { storesRouter } from "../procedures/stores.procedure";
+
+
 export const appRouter = createTRPCRouter({
   stores: storesRouter,
   products: productsRouter,
--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -4,6 +4,7 @@ import { cache } from "react";
 import { createTRPCContext } from "./init";
 import { makeQueryClient } from "./query-client";
 import { appRouter } from "./routers/_app";
+import { EventEmitter } from "stream";
 // IMPORTANT: Create a stable getter for the query client that
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
@@ -15,3 +16,6 @@ export const trpc = createTRPCOptionsProxy({
 
 // ...
 export const caller = appRouter.createCaller(createTRPCContext);
+
+
+export const ee = new EventEmitter()
